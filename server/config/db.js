e355be12e749ab492fcb64c409a1360444d3702d
thejusshaji <thejusshaji2000@gmail.com>
2023-03-03 19:22:35 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI" : "mongodb+srv://thejus:pHx1zYeJGjdpr3Ey@cluster0.04mokzz.mongodb.net/test"
+};
+
+
+/******************************
+ * File: db.js
+ * Student ID: 301301078
+ * Web app: Favourite Book List
+ ******************************/
