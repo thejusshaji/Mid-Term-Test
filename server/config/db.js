7cdf8d3e6165103d851b766467d916273be08b70
Thejus <66480094+thejusshaji@users.noreply.github.com>
2023-03-05 20:43:47 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI" : "mongodb+srv://thejus:ZDOhsvEFWdwNG3Ef@cluster0.04mokzz.mongodb.net/?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://moneyraheja:4vJQzuU9779E5mZ9@cluster0.ajkufxz.mongodb.net/?retryWrites=true&w=majority"
+  //"URI" : "mongodb+srv://thejus:ZDOhsvEFWdwNG3Ef@cluster0.04mokzz.mongodb.net/?retryWrites=true&w=majority"
   
   //"mongodb+srv://thejus:ZDOhsvEFWdwNG3Ef@cluster0.04mokzz.mongodb.net/?retryWrites=true&w=majority"
 };
